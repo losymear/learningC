a8a7811a2428eb8390631a8efe46979719118402
losymear <w15868821768@gmail.com>
2019-01-08 20:37:27 +0800

    CMakeLists.txt

---
+++ pat/Basic_Level_Pratice/1013.c
@@ -0,0 +1,9 @@
+/**
+* @program: Basic_Level_Pratice
+* @description: 
+* @author: mearlosy
+* @create: 2019-01-08 20:35
+*/
+
+
+
---
+++ pat/Basic_Level_Pratice/addFileCmd.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+for i in {14..95}
+do
+    cp 1013.c 10${i}.c
+done
+
