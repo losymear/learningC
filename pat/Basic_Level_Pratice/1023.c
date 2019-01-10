df4bacc1149d5404c37479c4dff0ba07d2017153
losymear <w15868821768@gmail.com>
2019-01-10 13:11:40 +0800

    1024

--- pat/Basic_Level_Pratice/1023.c
+++ pat/Basic_Level_Pratice/1023.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
 * @program: Basic_Level_Pratice
 * @description: 
@@ -7,6 +9,24 @@
 
 
 int main() {
-    
+    int numCounter[10] = {0};
+    int firstPrinted = 0;
+    for (int i = 0; i <= 9; ++i) {
+        scanf("%d", &numCounter[i]);
+        if (firstPrinted == 0 && i > 0 && numCounter[i] > 0) {
+            printf("%d", i);
+            numCounter[i]--;
+            firstPrinted = 1;
+        }
+    }
+
+
+    for (int j = 0; j <= 9; ++j) {
+        while (--numCounter[j] >= 0) {
+            printf("%d", j);
+        }
+    }
+
+
 }
 
--- pat/Basic_Level_Pratice/1024.c
+++ pat/Basic_Level_Pratice/1024.c
@@ -1,12 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+
 /**
 * @program: Basic_Level_Pratice
-* @description: 
+* @description:  科学计数法， 注意一些细节
 * @author: mearlosy
 * @create: 2019-01-08 20:35
 */
 
 
 int main() {
-    
+    char input[10001];
+    scanf("%s", input);
+    int n = (int) strlen(input);
+    if (input[0] == '-') printf("-"); // 首位是-则输出
+
+    int idx_dot = 2;
+
+    int idx_E = idx_dot;
+
+    while (input[++idx_E] != 'E') {
+    }
+
+    char symbolExp = input[idx_E + 1];
+
+    // 指数的结对值
+    int exp = 0;
+    for (int k = idx_E + 2; k < strlen(input); ++k) {
+        exp = 10 * exp + input[k] - '0';
+    }
+    // 指数为负数
+    if (symbolExp == '-') {
+        printf("0.");
+
+
+        for (int i = 0; i < exp - 1; ++i) {
+            printf("%d", 0);
+        }
+        printf("%c", input[idx_dot - 1]);
+
+        for (int j = idx_dot + 1; j < idx_E; ++j) {
+            printf("%c", input[j]);
+        }
+
+    }
+
+    // 指数为正数
+    if (symbolExp == '+') {
+        printf("%c", input[1]);
+        // 指数不足，仍有小数部分
+        if (exp < idx_E - 3) {
+            for (int i = 0; i < exp; ++i) {
+                printf("%c", input[3 + i]);
+            }
+            printf("%c", '.');
+            for (int j = exp + 3; j < idx_E; ++j) {
+                printf("%c", input[j]);
+
+            }
+            return 0;
+        }
+        for (int i = 3; i < idx_E; ++i) {
+            printf("%c", input[i]);
+        }
+        for (int i = idx_E - idx_dot; i <= exp; ++i) {
+            printf("%c", '0');
+        }
+    }
+
+
 }
+
 
